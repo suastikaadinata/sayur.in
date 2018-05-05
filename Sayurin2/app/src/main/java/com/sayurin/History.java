5d6eabfb4b65a0a5266b0f38c2240947016daacd
Unknown <erico.pangestu@yahoo.com>
2018-05-05 10:32:10 +0700

    Hisory fragment

--- Sayurin2/app/src/main/java/com/sayurin/History.java
+++ Sayurin2/app/src/main/java/com/sayurin/History.java
@@ -1,7 +1,14 @@
 package com.sayurin;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class History extends AppCompatActivity {
 
@@ -9,5 +16,36 @@ public class History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("History");
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout_history);
+        tabLayout.addTab(tabLayout.newTab().setText("IN PROGRESS"));
+        tabLayout.addTab(tabLayout.newTab().setText("Completed"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
+
 }
