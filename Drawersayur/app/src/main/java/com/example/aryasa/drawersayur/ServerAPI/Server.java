fbfc9b452e683f521c3b9995c37551cba9040f47
Nigel Erico Pangestu <erico.pangestu@yahoo.com>
2018-10-25 13:41:35 +0700

    activity + sequence

--- Drawersayur/app/src/main/java/com/example/aryasa/drawersayur/ServerAPI/Server.java
+++ Drawersayur/app/src/main/java/com/example/aryasa/drawersayur/ServerAPI/Server.java
@@ -5,8 +5,8 @@ public class Server{
     //untuk emu avd android menggunakan ip = http://10.0.2.2/
     //untuk langsung coba dihandphone menggunakan jaringan yang sama dan liat ipnya contoh http://192.168.1.14/
 
-    public static final String URL = "http://127.0.0.1:8000/api/";
-    public static final String URLIMAGE = "http://127.0.0.1:8000/img/";
+    public static final String URL = "http://10.0.2.2/api/";
+    public static final String URLIMAGE = "http://10.0.2.2/img/";
         //SharedPreferences sharedpreferences = getSharedPreferences(Login.my_shared_preferences2, Context.MODE_PRIVATE);
         //token = "Bearer "+ sharedpreferences.getString(Login.TAG_TOKEN, null);
 
