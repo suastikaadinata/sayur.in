bff4b85732281db1b75e82a8595d037b6fe6ca63
aryasa1 <di.aryasa@gmail.com>
2018-12-04 01:15:06 +0700

    help

--- Drawersayur/app/src/main/java/com/example/aryasa/drawersayur/Fragment/FragmentHelp.java
+++ Drawersayur/app/src/main/java/com/example/aryasa/drawersayur/Fragment/FragmentHelp.java
@@ -7,11 +7,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.aryasa.drawersayur.R;
+import com.github.barteksc.pdfviewer.PDFView;
 
 public class FragmentHelp extends Fragment {
+    PDFView pdfView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        final View view = inflater.inflate(R.layout.fragment_help, container, false);
+
+        pdfView = (PDFView) view.findViewById(R.id.pdfhelp);
+        pdfView.fromAsset("USER MANUAL.pdf").load();
+
+        return view;
     }
 }
